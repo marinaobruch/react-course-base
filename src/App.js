25a23bf2075051c6d310fb6ad4c4b9ec7398532e
Marina Obruch <marina_obruch@mail.ru>
2023-08-01 21:06:48 +0500

    Пример классового компонента

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <ClassCounter />
     </div>
   );
 }
