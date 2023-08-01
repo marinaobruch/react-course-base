43f6ec93adfc8aa7f2c31526ced258a6e0385fcf
Marina Obruch <marina_obruch@mail.ru>
2023-08-01 21:44:38 +0500

    Знакомство с props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
+      <PostItem post={{ id: 2, title: "HTML", body: "Description" }} />
+      <PostItem post={{ id: 3, title: "CSS", body: "Description" }} />
+      <PostItem post={{ id: 4, title: "React", body: "Description" }} />
+      <PostItem post={{ id: 5, title: "Python", body: "Description" }} />
     </div>
   );
 }
