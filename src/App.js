b967c18266b8a6feac03f18faab9baa697e366c7
Marina Obruch <marina_obruch@mail.ru>
2023-08-01 22:44:13 +0500

    Исп-е массива

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import "./styles/App.css";
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "HTML", body: "Description" },
+    { id: 3, title: "CSS", body: "Description" },
+    { id: 4, title: "React", body: "Description" },
+    { id: 5, title: "Python", body: "Description" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 6, title: "JavaScript", body: "Description" },
+    { id: 7, title: "HTML", body: "Description" },
+    { id: 8, title: "CSS", body: "Description" },
+    { id: 9, title: "React", body: "Description" },
+    { id: 10, title: "Python", body: "Description" },
+  ]);
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
-      <PostItem post={{ id: 2, title: "HTML", body: "Description" }} />
-      <PostItem post={{ id: 3, title: "CSS", body: "Description" }} />
-      <PostItem post={{ id: 4, title: "React", body: "Description" }} />
-      <PostItem post={{ id: 5, title: "Python", body: "Description" }} />
+      <PostList posts={posts} title={"Posts lists 1"} />
+      <PostList posts={posts2} title={"Posts lists 2"} />
     </div>
   );
 }
