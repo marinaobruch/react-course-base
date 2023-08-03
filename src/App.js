c4e6c60ebe8ced1b9f9d4f5397dd592cad1e55db
Marina Obruch <marina_obruch@mail.ru>
2023-08-03 20:07:32 +0500

    подключение формы ввода постов, постинг

--- src/App.js
+++ src/App.js
@@ -14,11 +14,18 @@ function App() {
   ]);
 
   const [title, setTitle] = useState("");
-  const bodyInputRef = useRef();
+  const [body, setBody] = useState("");
 
   const addNewPost = (e) => {
     e.preventDefault();
-    console.log(bodyInputRef.current.value);
+    const newPost = {
+      id: Date.now(),
+      title,
+      body,
+    };
+    setPosts([...posts, newPost]);
+    setTitle("");
+    setBody("");
   };
 
   return (
@@ -32,9 +39,9 @@ function App() {
           placeholder="Post name"
         />
 
-        {/* Неуправляемый/ неконтролируемый компонент */}
         <MyInput
-          ref={bodyInputRef}
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
           type="text"
           placeholder="Post discription"
         />
